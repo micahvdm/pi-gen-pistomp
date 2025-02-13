faab6056523bac8642da16307c87c634392e3061
DivideCo <micahvdm@gmail.com>
2025-02-13 16:06:40 +0200

    leave modtty out for now.
    I will build it as a deb and add to APT

--- stage2/05-pistomp/02-run.sh
+++ stage2/05-pistomp/02-run.sh
@@ -6,11 +6,6 @@ on_chroot << EOF
 mkdir -p /home/${FIRST_USER_NAME}/tmp
 cd /home/${FIRST_USER_NAME}/tmp
 
-git clone https://github.com/moddevices/mod-ttymidi.git
-cd mod-ttymidi
-make install
-cd ..
-
 wget http://download.drobilla.net/lilv-0.24.12.tar.bz2
 tar xvf lilv-0.24.12.tar.bz2
 cd lilv-0.24.12
